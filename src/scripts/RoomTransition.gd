4530f867b6689608eab62789aeafb055d7f1cfaf
jamnk <pancakes3825@gmail.com>
2023-02-22 01:02:14 -0500

    trance

--- src/scripts/RoomTransition.gd
+++ src/scripts/RoomTransition.gd
@@ -2,7 +2,11 @@ tool
 extends Area2D
 
 export(String, FILE) var next_room_path: = ""
-	
+export var spawn: int = 1
+
+func _ready():
+	GameData.spawnNum = spawn
+
 func _get_configuration_warning() -> String:
 	return "next_room_path PORTAL must be set dumbfuck" if next_room_path == "" else ""
 	
